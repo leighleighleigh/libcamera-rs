074d5f566126a6936c0a98d71d37c5576b23c4e1
Leigh Oliver <leighcharlesoliver@gmail.com>
2025-04-17 15:04:04 +1000

    Added missing geometry::Point import. Regenerated for v0.5.0

--- libcamera-meta/src/bin/generate_from_git.rs
+++ libcamera-meta/src/bin/generate_from_git.rs
@@ -429,7 +429,7 @@ mod generate_rust {
                 use crate::control::{{Control, Property, ControlEntry, DynControlEntry}};
                 use crate::control_value::{{ControlValue, ControlValueError}};
                 #[allow(unused_imports)]
-                use crate::geometry::{{Rectangle, Size}};
+                use crate::geometry::{{Point, Rectangle, Size}};
                 #[allow(unused_imports)]
                 use libcamera_sys::*;
 
--- regenerate.sh
+++ regenerate.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cargo run --bin generate_from_git
 
-cargo +nightly fmt --all
+cargo fmt --all
